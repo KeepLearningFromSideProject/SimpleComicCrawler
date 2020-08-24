4e7d8db54405ee8c4d24005c65c594e0240027cf
FATESAIKOU <qzsecftbhhhh@gmail.com>
2020-08-24 23:47:40 +0900

    Complete fuseDBDriver

---
+++ src/fuse_main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+A basic runner to crawl comic with the usage of fuse storage driver
+
+@argv[1]: the file path of request json
+@argv[2]: the directory of crawling scripts
+@argv[3]: the url of fuse db
+"""
+
+import sys
+import json
+
+from lib.request_handler import RequestHandler
+
+
+def process(raw_request, code_base_dir, db_url):
+    req_h = RequestHandler(
+        crawler_config={
+            'code_dir_base': code_base_dir,
+            'worker_info': {
+                'type': 'direct'
+            }
+        },
+        storage_config={
+            'type': 'fuse_db',
+            'db_url': db_url}
+    )
+
+    req_h.do(raw_request)
+    req_h.sync()
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as src:
+        raw_request = json.loads(src.read())
+
+    process(
+        raw_request=raw_request,
+        code_base_dir=sys.argv[2],
+        db_url=sys.argv[3]
+    )
--- src/lib/storage_engine.py
+++ src/lib/storage_engine.py
@@ -3,18 +3,13 @@ storage engine
 """
 
 import json
+import requests
 from .dataclasses.comic_data_class import ComicCase
 
 
 class StorageEngine:
     def __init__(self, config={'type': 'json', 'storage_path': '../db.json'}):
-        self.storage_type = config['type']
-        self.storage_path = config['storage_path']
-
-        self.storage = get_storage_instance(
-            storage_type=self.storage_type,
-            storage_path=self.storage_path
-        )
+        self.storage = get_storage_instance(config)
 
     def get_all(self):
         raw = self.storage.get_all()
@@ -38,8 +33,42 @@ class JsonStorageDriver:
             dst.write(json.dumps(raw, indent=4))
 
 
-def get_storage_instance(storage_type, storage_path):
-    if storage_type == 'json':
-        return JsonStorageDriver(storage_path)
+class FuseDBDriver:
+    def __init__(self, db_url):
+        self.db_url = db_url
+
+    def get_all(self):
+        all_raw = {}
+        comics = requests.get(self.db_url + "/list").json()['data']
+        for comic in comics:
+            all_raw[comic] = {}
+            episodes = requests.get(self.db_url + f"/list/{comic}").json()['data']
+            for episode in episodes:
+                all_raw[comic][episode] = [
+                    *requests.get(self.db_url + f"/list/{comic}/{episode}").json()['data']
+                ]
+
+        return all_raw
+
+    def save_all(self, raw):
+        db_raw = self.get_all()
+        new_raw = {}
+
+        for comic in raw:
+            new_raw[comic] = {}
+            for episode in raw[comic]:
+                if episode not in db_raw[comic]:
+                    new_raw[comic][episode] = raw[comic][episode]
+
+        requests.post(
+            self.db_url + '/add', json=raw
+        )
+
+
+def get_storage_instance(storage_config):
+    if storage_config["type"] == 'json':
+        return JsonStorageDriver(storage_config["storage_path"])
+    elif storage_config["type"] == 'fuse_db':
+        return FuseDBDriver(storage_config['db_url'])
     else:
-        raise NotImplemented(f'No such StorageDriver of type "{storage_type}"')
+        raise NotImplemented(f'No such StorageDriver of type "{storage_config["type"]}"')
