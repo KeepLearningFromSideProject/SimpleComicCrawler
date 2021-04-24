d31d1fa78c72a7230fe453c50998a70233723928
Kou19808 <kou19808@cs.saisoncard.co.jp>
2021-04-25 01:01:07 +0900

    Add no-sandbox for get_image script

--- scripts/get_images.py
+++ scripts/get_images.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 def doRequest(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--proxy-server=socks5://localhost:8080')
 
     driver = webdriver.Chrome(options=chrome_options)
