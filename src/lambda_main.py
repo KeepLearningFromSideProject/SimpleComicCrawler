3f21fab9fab45e06ff669cfbb9fd65a08013da34
FATESAIKOU <qzsecftbhhhh@gmail.com>
2021-04-26 02:20:58 +0900

    Alter lambda_main to check proxy

--- src/lambda_main.py
+++ src/lambda_main.py
@@ -1,11 +1,17 @@
 import logging
 import json
+import time
+import subprocess
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
 def handler(event, context):
     LOGGER.info('Event: %s', event)
+
+    proxy_process = subprocess.Popen("/proxy_launch.sh")
+    time.sleep(10)
+    proxy_process.terminate()
 
     return {
         'statusCode': 200,
