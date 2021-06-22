65705d26a6fa262e7e9bd821f66f409e7ffbf3c0
FATESAIKOU <qzsecftbhhhh@gmail.com>
2021-06-23 00:27:39 +0900

    Remove unnecessary test and fix crawling test

--- tests/crawling_doer_tests.py
+++ tests/crawling_doer_tests.py
@@ -8,40 +8,71 @@ from lib.dataclasses.download_request import BasicDownloadRequest
 from lib.crawl_engine import CrawlingDoer
 
 
-class CodeGeneratorTest(unittest.TestCase):
+class MockedStorageEngine():
+    def __init__(self, comic_case=ComicCase({})):
+        self.cc = comic_case
+
+    def get_all(self):
+        return self.cc.to_raw()
+
+    def save_all(self, comic_case):
+        ori_raw = self.cc.to_raw()
+        new_raw = comic_case.to_raw()
+
+        for comic in new_raw:
+            if comic not in ori_raw:
+                ori_raw[comic] = {}
+
+            for episode in new_raw[comic]:
+                ori_raw[comic][episode] = new_raw[comic][episode]
+
+        self.cc.from_raw(ori_raw)
+
+    def get_comic_case(self):
+        return self.cc
+
+class CrawlingDoerTest(unittest.TestCase):
     def setUp(self):
         self.comic_case1 = ComicCase({'食戟之靈': {}})
         self.comic_case2 = ComicCase({'食戟之靈': {'01話': []}})
         self.comic_case3 = ComicCase({'食戟之靈': {'01話': ['testurl1.jpg']}})
 
         self.download_request = BasicDownloadRequest({
-            '食戟之靈': {'01話': [], '02話': []}
+            '食戟之靈': {'01話': []}
         })
 
     def tearDown(self):
         pass
 
     def test_get_image1(self):
+        se  = MockedStorageEngine(self.comic_case1)
         cd1 = CrawlingDoer(self.comic_case1, self.download_request,
-                           config={'code_dir_base': 'scripts', 'worker_info': {'type': 'direct'}})
-        r_cc1 = cd1.do_request()
+                           config={'code_dir_base': 'scripts', 'worker_info': {'type': 'direct'}, 'max_download': 0},
+                           storage_engine=se)
+        cd1.do_request()
+        r_cc1 = se.get_comic_case()
 
         assert r_cc1.get('食戟之靈').get('01話').data != []
-        assert r_cc1.get('食戟之靈').get('02話').data != []
 
     def test_get_image2(self):
+        se  = MockedStorageEngine(self.comic_case2)
         cd2 = CrawlingDoer(self.comic_case2, self.download_request,
-                           config={'code_dir_base': 'scripts', 'worker_info': {'type': 'direct'}})
-        r_cc2 = cd2.do_request()
+                           config={'code_dir_base': 'scripts', 'worker_info': {'type': 'direct'}, 'max_download': 0},
+                           storage_engine=se)
+        cd2.do_request()
+        r_cc2 = se.get_comic_case()
+
         assert r_cc2.get('食戟之靈').get('01話').data != []
-        assert r_cc2.get('食戟之靈').get('02話').data != []
 
     def test_get_image3(self):
+        se  = MockedStorageEngine(self.comic_case3)
         cd3 = CrawlingDoer(self.comic_case3, self.download_request,
-                           config={'code_dir_base': 'scripts', 'worker_info': {'type': 'direct'}})
-        r_cc3 = cd3.do_request()
-        assert r_cc3.get('食戟之靈').get('01話').data == self.comic_case3.get('食戟之靈').get('01話').data
-        assert r_cc3.get('食戟之靈').get('02話').data != []
+                           config={'code_dir_base': 'scripts', 'worker_info': {'type': 'direct'}, 'max_download': 0},
+                           storage_engine=se)
+        cd3.do_request()
+        r_cc3 = se.get_comic_case()
+
+        assert set(r_cc3.get('食戟之靈').get('01話').data) == set(self.comic_case3.get('食戟之靈').get('01話').data)
 
 
 if __name__ == '__main__':
