21b24cfe5d8e6224dfea41510eae2e66ca5632a1
FATESAIKOU <qzsecftbhhhh@gmail.com>
2021-07-21 23:45:44 +0900

    Add ec2 as db to terraform v3

---
+++ env/v3_lambda_ec2/variables.tf
@@ -0,0 +1,25 @@
+
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {} 
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "db_username" {}
+variable "db_password" {}
+variable "db_sql_path" {}
+
+variable "region" {
+    default = "ap-northeast-1"
+}
+
+variable "network_address_space" {
+    default = "10.1.0.0/16"
+}
+
+variable "subnet1_address_space" {
+    default = "10.1.0.0/24"
+}
+
